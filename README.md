9d7b8a7e276e40b3abe71bd3c6f924eb6f1022f9
zentient <67522653+zentient@users.noreply.github.com>
2020-12-17 09:30:52 -0400

    Flutter Project #1
    - Up to Lesson 28: I Am Rich. (Completed!)

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter-Project-One
+ Flutter Project #1 - Up to Lesson 28: I Am Rich. (Completed!)
---
+++ android/app/src/main/kotlin/com/example/project_one_iamrich/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_one_iamrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
